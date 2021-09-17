143874b8b0c7f776df665ab299fba0b7a9f3cd86
ilosrim <mirsolikhdev@gmail.com>
2021-09-16 23:41:58 -0400

    Build header section

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Headers, Title, Desc, InputBox, Input, ArrowBox, Commit } from './style'
+import { ArrowRight } from '@styled-icons/bootstrap/ArrowRight'
+import HeaderLine from '../../images/header/header-line.png'
 
 export const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <Headers>
+      <Title>Ready to become a better product designer?</Title>
+      <Desc>Learn advanced design tactics with hands-on lessons delivered directly inside of Figma</Desc>
+      <InputBox>
+        <Input placeholder='Your@email.com' />
+        <ArrowBox>
+          <ArrowRight />
+        </ArrowBox>
+      </InputBox>
+      <Commit>
+        <img src={HeaderLine} alt='line' />
+        <p>Get notified when enrollment opens again.</p>
+      </Commit>
+    </Headers>
   )
 }
---
+++ src/components/header/style.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import HeaderLogo from '../../images/header/header.png'
+
+export const Headers = styled.div`
+  width: 1152px;
+  height: 900px;
+  background-image: url(${HeaderLogo});
+  background-size: cover;
+  background-position: center;
+  margin: 55px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-left: 217px;
+  padding-right: 217px;
+`
+export const Title = styled.div`
+  font-size: 64px;
+  line-height: 68px;
+  text-align: center;
+  color: #090B37;
+`
+export const Desc = styled.div`
+  font-size: 22px;
+  line-height: 32px;
+  text-align: center;
+  color: #3C2B84;
+  margin: 28px 0 34px;
+`
+export const InputBox = styled.div`
+  height: 80px;
+  width: 640px;
+  background: #FFFFFF;
+  border: 2px solid #090B37;
+  box-sizing: border-box;
+  box-shadow: 1px 1px 0px #090B37;
+  border-radius: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 27px 8px 27px 38px;
+`
+export const Input = styled.input`
+  font-size: 17px;
+  line-height: 26px;
+  text-decoration-line: underline;
+  color: #A0A0A0;
+  border: none;
+  outline: none;
+`
+export const ArrowBox = styled.div`
+  width: 120px;
+  height: 64px;
+  background: #090B37;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: all 0.3s linear;
+  :active{
+    transform: scale(0.8);
+  }
+  svg{
+    width: 24px;
+    color: #fff;
+  }
+`
+export const Commit = styled.div`
+  width: 608px;
+  display: flex;
+  padding-left: 15px;
+  p{
+    font-size: 17px;
+    line-height: 26px;
+    color: #3C2B84;
+    padding: 23px 0 0 15px;
+  }
+`
