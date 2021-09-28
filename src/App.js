a397c93eed006ffecab031e29131209b70e8e81f
ilosrim <mirsolikhdev@gmail.com>
2021-09-28 16:47:33 +0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './App.css'
+import { Active } from './components/active';
 import {Carousel} from './components/carousel/index'
 import { Commits } from './components/commits';
-
 import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { Navbar } from './components/navbar';
 import {Works} from './components/works'
-// import {Active} from './components/active/index'
 
 function App() {
   return (
@@ -15,6 +14,7 @@ function App() {
       <Navbar />
       <Header />
       <Works />
+      <Active />
       <Carousel />
       <Commits />
       <Footer />
---
+++ src/active2/active/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+// import {classes} from './style.mudule.css'
+import Rastangle from '../../images/active/Rectangle.png'
+// import Vest from '../../images/active/Vect.png'
+import cycle from '../../images/works/vektor/Vector1.png'
+import bur from '../../images/works/vektor/Vector2.png'
+import right from '../../images/works/vektor/Vector3.png'
+import './style.css'
+
+function index() {
+    return (
+        <div className="active">
+            <div className="active__container">
+                <div className="active-info">
+                    <p>ACTIVE LEARNING</p>
+                    <span>Improve the skills you use every day as a product designer</span>
+                    <div className="active-bottom-info">
+                        <div className="v-collect">
+                            <img src={cycle} alt="logo" className="v__item1" ></img>
+                            <img src={bur} alt="logo" className="v__item2" ></img>
+                            <img src={right} alt="logo" className="v__item3" ></img>
+                        </div>
+                        <p>Our curriculum walks you through each step of designing a new product from scratch. You'll learn a ton of practical strategies to improve your daily workflow and dive deep into the rabbit hole of Figma and all it has to offer.</p>
+                    </div>
+                </div>
+                <div className="active-cart">
+                    <ul className="active-collect">
+                        <li className="active-collect-item1"><p>01</p><span>Design-Systems</span></li>
+                        <li className="active-collect-item2"><p>02</p><span>Practical-UI</span></li>
+                        <li className="active-collect-item3"><p>03</p><span>Responsive</span></li>
+                        <li className="active-collect-item4"><p>04</p><span>Handoff</span></li>
+                        <li className="active-collect-item5"><p>05</p><span>Prototyping</span></li>
+                        <li className="active-collect-item6"><p>06</p><span>Workflow</span></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="active__footerImg">
+            </div>
+            <img src={Rastangle} alt="" className="active-footer-logo" />
+        </div>
+    )
+}
+
+export default index
+// <img src={Vest} alt="" className="active-logo" />
